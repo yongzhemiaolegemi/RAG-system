87cbb95917a8c643fcd84b2b66e25387e82004a0
wlc000 <13681161371@163.com>
2025-07-29 12:47:22 +0800

    新增用于deep_research的prompt

--- LightRAG/lightrag/prompt.py
+++ LightRAG/lightrag/prompt.py
@@ -242,6 +242,43 @@ When handling relationships with timestamps:
 
 Response:"""
 
+PROMPTS["rag_response_deep_research"] = """---Role---
+
+You are a helpful assistant responding to user query about Knowledge Graph and Document Chunks provided in JSON format below.
+
+
+---Goal---
+
+Generate a concise response based on Knowledge Base and follow Response Rules, considering both the conversation history and the current query. Summarize all information in the provided Knowledge Base, and incorporating general knowledge relevant to the Knowledge Base. Do not include information not provided by Knowledge Base.
+
+When handling relationships with timestamps:
+1. Each relationship has a "created_at" timestamp indicating when we acquired this knowledge
+2. When encountering conflicting relationships, consider both the semantic content and the timestamp
+3. Don't automatically prefer the most recently created relationships - use judgment based on the context
+4. For time-specific queries, prioritize temporal information in the content before considering creation timestamps
+
+---Conversation History---
+{history}
+
+---Knowledge Graph and Document Chunks---
+{context_data}
+
+---Response Rules---
+
+- Target format and length: {response_type}
+- Use markdown formatting with appropriate section headings
+- Please respond in the same language as the user's question.
+- Ensure the response maintains continuity with the conversation history.
+- Mark the reference content when your generated content use it. Clearly indicating whether each source is from Entities (E), Relationships (R) or Document Chunks (DC), and include the id in the following format: [E/R/DC #id].
+- Do not list references at the end.
+- If you don't know the answer, just say so.
+- Do not make anything up. Do not include information not provided by the Knowledge Base.
+- Addtional user prompt: {user_prompt}
+
+Response:"""
+
+
+
 PROMPTS["keywords_extraction"] = """---Role---
 
 You are a helpful assistant tasked with identifying both high-level and low-level keywords in the user's query and conversation history.
