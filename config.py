b1e048f240facf30f45cd5b4b5d51fde848f20c9
YucYux <58339904+YucYux@users.noreply.github.com>
2025-08-03 20:07:38 +0800

    deep research model can be changed in config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ lightrag_llm_model = 'doubao-1-5-lite-32k-250115'
 lightrag_embedding_model = 'doubao-embedding-text-240715'
 
 lightrag_llm_url = 'https://ark.cn-beijing.volces.com/api/v3/'
-lightrag_llm_key = 'f0152ae2-7a94-4259-beaa-bc5ade605c76'
+lightrag_llm_key = ''
 
 # The model selection for LightRAG is hardcoded in the code as gpt-4o-mini, may be changed later.
 lightrag_service_port = 5001
@@ -30,8 +30,12 @@ webscrap_base_dir = 'ooo'
 # Django configuration
 
 django_llm_url = "https://api.chatanywhere.tech/v1"    
-django_llm_key = "sk-MTfzmlK28vjHLCY1f72UVD7tlu0UPnt8Gd9GHnttyQsGosal"
+django_llm_key = ""
 django_vllm_url = "http://localhost:30000/v1/completions"
 django_model ="deepseek-chat"
 django_service_port = '8006'
 django_service_url = f"http://127.0.0.1:{django_service_port}/api/v1/chat/completions"
+
+# DeepResearch config
+dr_model = "qwen3-235b-a22b-instruct-2507"
+# dr_llm_url&key is django's
